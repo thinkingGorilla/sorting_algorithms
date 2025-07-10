aecd0776a2ddb1c3d35774c1ac0f68d6cac557e0
thinkingGorilla <4zxzxz4@gmail.com>
2025-07-10 21:55:34 +0900

    코드 리팩토링
    - 선택정렬, 삽입정렬 코드에 오름차순/내림차순 함수형 적용

--- src/main/java/me/thinking_gorilla/InsertionSort.java
+++ src/main/java/me/thinking_gorilla/InsertionSort.java
@@ -1,20 +1,29 @@
 package me.thinking_gorilla;
 
+import java.util.function.BiPredicate;
+
 import static me.thinking_gorilla.Utils.print;
 
 public class InsertionSort {
 
-    // 삽입 정렬 메소드
-    public static void insertionSort(int[] array) {
-        int n = array.length;
+    public static void ascendingSort(int[] array) {
+        insertionSort(array, (current, key) -> current > key);
+    }
 
+    public static void descendingSort(int[] array) {
+        insertionSort(array, (current, key) -> current < key);
+    }
+
+    public static void insertionSort(int[] array, BiPredicate<Integer, Integer> condition) {
         // 배열의 두 번째 요소부터 시작
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < array.length; i++) {
+            // array[j + 1] = array[j]이 수행되더라도 key가 변경되지 않도록
+            // 별도의 로컬 변수에 저장한다.
             int key = array[i];
             int j = i - 1;
 
             // key보다 큰 값을 만나면 해당 값을 오른쪽으로 이동
-            while (j >= 0 && array[j] > key) {
+            while (j >= 0 && condition.test(array[j], key)) {
                 array[j + 1] = array[j];
                 j--;
             }
@@ -24,12 +33,12 @@ public class InsertionSort {
         }
     }
 
-    // 메인 메소드 (테스트용)
     public static void main(String[] args) {
         int[] array = {64, 25, 12, 22, 11};
 
         print("삽입 정렬 전:", array);
-        insertionSort(array);
+        ascendingSort(array);
+        // descendingSort(array);
         print("삽입 정렬 후:", array);
     }
 }
--- src/main/java/me/thinking_gorilla/SelectionSort.java
+++ src/main/java/me/thinking_gorilla/SelectionSort.java
@@ -1,35 +1,41 @@
 package me.thinking_gorilla;
 
+import java.util.function.BiPredicate;
+
 import static me.thinking_gorilla.Utils.print;
 import static me.thinking_gorilla.Utils.swap;
 
 public class SelectionSort {
 
-    // 선택 정렬 메소드
-    public static void selectionSort(int[] array) {
-        int n = array.length;
+    public static void ascendingSort(int[] array) {
+        selectionSort(array, (current, extreme) -> current < extreme);
+    }
 
-        // 배열의 모든 요소에 대해 반복
-        for (int i = 0; i < n - 1; i++) {
-            // i부터 n까지의 배열 중에서 최소값을 찾음
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j; // 최소값의 인덱스를 업데이트
+    public static void descendingSort(int[] array) {
+        selectionSort(array, (current, extreme) -> current > extreme);
+    }
+
+    private static void selectionSort(int[] array, BiPredicate<Integer, Integer> condition) {
+        for (int i = 0; i < array.length - 1; i++) {
+
+            int extremeIndex = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (condition.test(array[j], array[extremeIndex])) {
+                    extremeIndex = j;
                 }
             }
 
-            // 최소값을 현재 위치로 교환
-            swap(array, minIndex, i);
+            swap(array, extremeIndex, i);
         }
     }
 
-    // 메인 메소드 (테스트용)
     public static void main(String[] args) {
         int[] array = {64, 25, 12, 22, 11};
 
         print("선택 정렬 전:", array);
-        selectionSort(array);
+        ascendingSort(array);
+        // descendingSort(array);
         print("선택 정렬 후:", array);
     }
 }
